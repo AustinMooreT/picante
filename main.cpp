1d25144b931aa6d99d2a80ec014766ae1165c52f
Austin Moore <mooreat2@miamioh.edu>
2023-12-18 21:58:06 -0500

    Create a vk instance object

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <functional>
+
 #include <vulkan/vulkan.hpp>
 
 constexpr vk::ApplicationInfo create_application_info() {
@@ -8,6 +10,22 @@ constexpr vk::ApplicationInfo create_application_info() {
   application_info.apiVersion         = 0;
   return application_info;
 };
+
+vk::UniqueInstance create_instance() {
+  const auto application_info = create_application_info();
+  const auto instance_info    = std::invoke([&application_info]() {
+    auto instance_info              = vk::InstanceCreateInfo{};
+    instance_info.pApplicationInfo  = &application_info;
+    instance_info.enabledLayerCount = 0;
+    // TODO I think I need certain layers for present and stuff
+    instance_info.ppEnabledLayerNames     = nullptr;
+    instance_info.enabledExtensionCount   = 0;
+    instance_info.ppEnabledExtensionNames = nullptr;
+    return instance_info;
+  });
+  return vk::createInstanceUnique(instance_info);
+}
+
 int main() {
   return 0;
 }
