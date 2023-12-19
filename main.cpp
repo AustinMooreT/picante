e05fcacb3662f18466905f98dccb737a42789e50
Austin Moore <mooreat2@miamioh.edu>
2023-12-18 21:52:26 -0500

    Setup application info

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+#include <vulkan/vulkan.hpp>
+
+constexpr vk::ApplicationInfo create_application_info() {
+  auto application_info               = vk::ApplicationInfo{};
+  application_info.pApplicationName   = "picante";
+  application_info.applicationVersion = 0;
+  application_info.pEngineName        = "picante";
+  application_info.apiVersion         = 0;
+  return application_info;
+};
 int main() {
   return 0;
 }
